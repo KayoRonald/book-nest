a15858ac5111511c3db44a7b1658a51e5a7ebcd8
KayoRonald <kayoronald4321@gmail.com>
2024-01-31 11:33:12 +0000

    feat: init first migration

---
+++ prisma/migrations/20240131102949_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Book" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "thumbl" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" REAL NOT NULL
+);
