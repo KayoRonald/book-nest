156accd7513a18c1495ab9e21fa91fddaba8ac14
KayoRonald <kayoronald4321@gmail.com>
2024-01-31 11:28:48 +0000

    feat: swagger config update

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,7 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+
+export const config = new DocumentBuilder()
+  .setTitle('Book test in nest')
+  .setDescription('The book API description')
+  .setVersion('1.0')
+  .build();
